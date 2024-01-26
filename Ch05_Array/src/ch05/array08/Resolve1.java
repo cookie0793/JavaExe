e61ed70d30ba55fbb2a62a64551782f68a0c3e8d
NCP08 <navercloudPlatform08.gmail.com>
2024-01-26 18:00:49 +0900

    1/26 최종업데이트

---
+++ Ch05_Array/src/ch05/array08/Resolve1.java
@@ -0,0 +1,17 @@
+package ch05.array08;
+
+//1. 정수 배열 5개를 할당하고
+//Arrays.sort를 활용해서 최소값과 최대값을 얻으세요
+
+import java.util.Arrays;
+
+public class Resolve1 {
+	public static void main(String[] args) {
+		int[] scores = {3, 2, 5, 4, 1};
+		Arrays.sort(scores);
+		
+		for(int i=1;i<scores.length;i++) {
+			
+		}
+	}
+}
