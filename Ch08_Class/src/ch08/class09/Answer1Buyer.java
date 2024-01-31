060f3270785bb7f05887c1cde16655083908a025
NCP08 <navercloudPlatform08.gmail.com>
2024-01-31 18:06:41 +0900

    1/31 끝

--- Ch08_Class/src/ch08/class09/Answer1Buyer.java
+++ Ch08_Class/src/ch08/class09/Answer1Buyer.java
@@ -1,5 +1,23 @@
 package ch08.class09;
 
-public class Answer1Buyer {
+import ch08.class09.Answer1Seller;
 
+public class Answer1Buyer {
+	private int money;
+	private int numOfApple;
+	
+	public Answer1Buyer(int money) {
+		this.money = money;
+		this.numOfApple = 0;
+	}
+	public void buyApple(Answer1Seller seller, int money) {
+		this.numOfApple += seller.saleApple(money);
+		this.money -= money;
+	}
+	public void showBuyResult() {
+		System.out.println("*** 과일 구매자의 현재 상황 ***");
+		System.out.println("현재 잔액 : " + money);
+		System.out.println("사과 개수 : " + numOfApple);
+	}
 }
+
--- Ch08_Class/src/ch08/class09/Answer1Main.java
+++ Ch08_Class/src/ch08/class09/Answer1Main.java
@@ -1,5 +1,18 @@
 package ch08.class09;
 
-public class Answer1Main {
+import ch08.class09.Answer1Buyer;
 
+public class Answer1Main {
+	public static void main(String[] args) {
+		Answer1Seller seller = new Answer1Seller(20);
+		Answer1Buyer buyer = new Answer1Buyer(10000);
+		
+		seller.showSaleResult();
+		buyer.showBuyResult();
+		
+		seller.saleApple(buyer, 2000);
+		
+		seller.showSaleResult();
+		buyer.showBuyResult();
+	}
 }
