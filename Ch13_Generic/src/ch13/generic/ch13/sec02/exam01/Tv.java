49723ac6c952ecf25e3209a2c15274cd7733700a
NCP08 <navercloudPlatform08.gmail.com>
2024-02-13 10:03:22 +0900

    2024/02/13

--- Ch13_Generic/src/ch13/generic/ch13/sec02/exam01/Tv.java
+++
@@ -1,5 +0,0 @@
-package ch13.generic.ch13.sec02.exam01;
-
-public class Tv {
-
-}
--- Ch13_Generic/src/ch13/generic/ch13/sec03/exam01/GenericExample.java
+++
@@ -1,6 +0,0 @@
-package ch13.generic.ch13.sec03.exam01;
-
-public class GenericExample {
-	//제네릭 메소드
-	public static <T>
-}
